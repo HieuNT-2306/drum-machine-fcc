b38ee6da2377378ebdc50eaa9f32559fbbc17f65
HieuNT-2306 <hieubeo2369@gmail.com>
2022-11-30 19:36:16 +0700

    completed 2

---
+++ src/drumPad.js
@@ -0,0 +1,57 @@
+const drumPad = [
+    {
+        key: "Q",
+        keycode: 81,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+        name: "Heater-1"
+    },
+    {
+        key: "W",
+        keycode: 87,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+        name: "Heater-2"
+    },
+    {
+        key: "E",
+        keycode: 69,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+        name: "Heater-3"
+    },
+    {
+        key: "A",
+        keycode: 65,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+        name: "Heater-4"
+    },
+    {
+        key: "S",
+        keycode: 83,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+        name: "Clap"
+    },
+    {
+        key: "D",
+        keycode: 68,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+        name: "Open-HH"
+    },
+    {
+        key: "Z",
+        keycode: 90,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+        name: "Kick n Hat"
+    },
+    {
+        key: "X",
+        keycode: 88,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+        name: "Kick"
+    },
+    {
+        key: "C",
+        keycode: 67,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+        name: "Closed-HH"
+    },
+]
+  export {drumPad};
---
+++ src/pianoPad.js
@@ -0,0 +1,57 @@
+const pianoPad = [
+    {
+        key: "Q",
+        keycode: 81,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+        name: "Chord-1"
+    },
+    {
+        key: "W",
+        keycode: 87,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+        name: "Chord-2"
+    },
+    {
+        key: "E",
+        keycode: 69,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+        name: "Chord-3"
+    },
+    {
+        key: "A",
+        keycode: 65,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+        name: "Shaker"
+    },
+    {
+        key: "S",
+        keycode: 83,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+        name: "Open-HH"
+    },
+    {
+        key: "D",
+        keycode: 68,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp",
+        name: "Closed-HH"
+    },
+    {
+        key: "Z",
+        keycode: 90,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+        name: "Punchy Kick"
+    },
+    {
+        key: "X",
+        keycode: 88,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+        name: "Side stick"
+    },
+    {
+        key: "C",
+        keycode: 67,
+        audio: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+        name: "Snare"
+    },
+]
+  export {pianoPad};
